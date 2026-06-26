7d4c6e60bfec11459056dfbfdb449bdd3a4a3cd3
brittonr <b@robitzs.ch>
2026-06-25 22:38:57 -0400

    Octet burn-down 6706 -> 6704

--- src/plugin/host.rs
+++ src/plugin/host.rs
@@ -7,7 +7,6 @@
 use std::path::Path;
 
 use preserves::IOValue;
-use preserves::Record;
 use preserves::Value;
 
 use crate::artifacts;
@@ -1360,7 +1359,7 @@ fn simple_record<'a>(
     value: &'a IOValue,
     label: &str,
     arity: usize,
-) -> Result<std::borrow::Cow<'a, Record<Value<IOValue>>>> {
+) -> Result<std::borrow::Cow<'a, preserves::Record<Value<IOValue>>>> {
     value
         .collect_simple_record(label, Some(arity))
         .ok_or_else(|| MoltenError::invalid_harness(format!("expected <{label} ...> with arity {arity}")))
